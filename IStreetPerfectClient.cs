4e4830b675f06ce88ac51c2104ade4eaabd6f773
Bill Miller <bmiller@postmedia.com>
2021-11-30 17:12:14 -0500

    rest client

---
+++ IStreetPerfectClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StreetPerfect.Models;
+
+namespace StreetPerfect
+{
+
+
+	public interface IStreetPerfectClient
+	{
+		string ConnectionString { get; set; }
+		bool Debug { get; set; }
+
+		GetInfoResponse GetInfo();
+
+		caQueryResponse caQuery(caQueryRequest req);
+		caFetchAddressResponse caFetchAddress(caFetchAddressRequest req);
+		caFormatAddressResponse caFormatAddress(caFormatAddressRequest req);
+		caValidateAddressResponse caValidateAddress(caValidateAddressRequest req);
+		caCorrectionResponse caProcessCorrection(caAddressRequest req);
+		caParseResponse caProcessParse(caAddressRequest req);
+		caSearchResponse caProcessSearch(caAddressRequest req);
+
+		usCorrectionResponse usProcessCorrection(usAddressRequest req);
+		usParseResponse usProcessParse(usAddressRequest req);
+		usSearchResponse usProcessSearch(usAddressRequest req);
+		usDeliveryInformationResponse usProcessDeliveryInfo(usAddressRequest req);
+	}
+
+}
