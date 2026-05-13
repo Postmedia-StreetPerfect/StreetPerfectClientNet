73f0f8677fac4fe41463c7ec49a6558fd3ba3554
Bill Miller <bmiller@postmedia.com>
2026-05-13 14:29:20 -0400

    citysearch

--- Controllers/Abstract/CA_TypeaheadController.cs
+++ Controllers/Abstract/CA_TypeaheadController.cs
@@ -93,41 +93,59 @@ namespace StreetPerfect.Controllers
 			}
 		}
 
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpPost("city")]
+        public ActionResult<caTypeaheadCityResponse> ca_city_search([FromBody] caTypeaheadCityRequest req)
+        {
+            try
+            {
+                var resp = _SpCaInterface.CitySearch(req);
+                EndpointSuccessful();
+                return resp;
+            }
+            catch (Exception ex)
+            {
+                //EndpointException(ex, req);
+                _logger.LogError("ca_city_search error, {m}", ex.Message);
+                return StatusCode(502, new { err = ex.Message });
+            }
+        }
 
-		/// <summary>
-		/// 
-		/// Query a Canadian address, handles partial address, city and province
-		///
-		/// </summary>
-		/// <remarks>
-		/// ### Typeahead/Autocomplete Query (returns full caAddress per hit)
-		/// There are two variations of the type of search for this function.
-		/// 1. Hard format address query, user must enter the first n characters of a formatted address line ([st/civic num] [st name] [st type] [direction] [city] [prov] [postal code]).
-		/// 2. Tokenized query, where each word the user enters will be used as a prefix search in the address line in any location.
-		/// 
-		/// The civic number is optional. Without it, searches will start at the street name or anywhere in the address if tokenized search is enabled, all without civic number restrictions.
-		/// The civic number will be parsed and returned in the result.
-		/// 
-		/// If a street suffix is appended to the civic number (no spaces) then the suffix will be parsed out and returned. No search restriction will be added for a suffix.  
-		/// Note the 3 fraction suffixes (1/4, 1/2, and 3/4) can also be used but must have at least one space between the civic number and the suffix.
-		/// 
-		/// A unit number can be prepended to the civic number separated by a hyphen (Canada Post syntax style) uuu-cccc where uuu is the unit number and cccc is the civic number.
-		/// The unit number will be parsed and returned in the result but will not have a search restriction placed on it.
-		/// 
-		/// You can further restrict the queries by adding city and/or province to the request. Note that city searches are prefixed (like a trailing wildcard; miss*)
-		/// and dashes are ignored so users can use them or not. The province must match as 2 letter form.
-		/// 
-		/// The difference between this api and the regular typeahead/ api, is that this api returns full caAddress records in the response. Whereas typeahead/ returns a formatted single line address for diaplay.
-		/// 
-		/// The response will contain any parsed values along with a list of caAddress records. Each caAddress record will also have a respective ID assigned to it. 
-		/// You can pass this ID to the typeahead/fetch api to return a properly formatted address. Although you could also simply call ca/correct yourself to format/optimize an address record.
-		///		
-		/// </remarks>
-		/// <param name="req">A caTypeaheadRequest object</param>
-		/// <response code="200">Returns caTypeaheadResponse</response>
-		/// <response code="400">If invalid request object</response>   
-		/// <response code="502">StreetPerfect API error</response>   
-		[HttpPost("rec")]
+
+        /// <summary>
+        /// 
+        /// Query a Canadian address, handles partial address, city and province
+        ///
+        /// </summary>
+        /// <remarks>
+        /// ### Typeahead/Autocomplete Query (returns full caAddress per hit)
+        /// There are two variations of the type of search for this function.
+        /// 1. Hard format address query, user must enter the first n characters of a formatted address line ([st/civic num] [st name] [st type] [direction] [city] [prov] [postal code]).
+        /// 2. Tokenized query, where each word the user enters will be used as a prefix search in the address line in any location.
+        /// 
+        /// The civic number is optional. Without it, searches will start at the street name or anywhere in the address if tokenized search is enabled, all without civic number restrictions.
+        /// The civic number will be parsed and returned in the result.
+        /// 
+        /// If a street suffix is appended to the civic number (no spaces) then the suffix will be parsed out and returned. No search restriction will be added for a suffix.  
+        /// Note the 3 fraction suffixes (1/4, 1/2, and 3/4) can also be used but must have at least one space between the civic number and the suffix.
+        /// 
+        /// A unit number can be prepended to the civic number separated by a hyphen (Canada Post syntax style) uuu-cccc where uuu is the unit number and cccc is the civic number.
+        /// The unit number will be parsed and returned in the result but will not have a search restriction placed on it.
+        /// 
+        /// You can further restrict the queries by adding city and/or province to the request. Note that city searches are prefixed (like a trailing wildcard; miss*)
+        /// and dashes are ignored so users can use them or not. The province must match as 2 letter form.
+        /// 
+        /// The difference between this api and the regular typeahead/ api, is that this api returns full caAddress records in the response. Whereas typeahead/ returns a formatted single line address for diaplay.
+        /// 
+        /// The response will contain any parsed values along with a list of caAddress records. Each caAddress record will also have a respective ID assigned to it. 
+        /// You can pass this ID to the typeahead/fetch api to return a properly formatted address. Although you could also simply call ca/correct yourself to format/optimize an address record.
+        ///		
+        /// </remarks>
+        /// <param name="req">A caTypeaheadRequest object</param>
+        /// <response code="200">Returns caTypeaheadResponse</response>
+        /// <response code="400">If invalid request object</response>   
+        /// <response code="502">StreetPerfect API error</response>   
+        [HttpPost("rec")]
 		public ActionResult<caTypeaheadResponse> ca_typeahead_rec([FromBody] caTypeaheadRequest req)
 		{
 			try
